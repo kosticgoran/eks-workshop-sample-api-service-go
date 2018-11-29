dc031164b9a64c95af1ead8b7a305d06442f7b0e
kosticgoran <kosticgoran@rocketmail.com>
2018-11-29 12:57:05 -0800

    added gorkos 2
    added gorkos 2 ..

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World gorkos"}
+		res := &response{Message: "Hello World gorkos 2"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
